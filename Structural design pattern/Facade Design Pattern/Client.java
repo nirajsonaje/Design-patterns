1a23cd58f89b4b6973304cd7146da934bbdce0de
Niraj sonaje <101004260+nirajsonaje@users.noreply.github.com>
2025-07-24 17:36:12 +0530

    Update Client.java

--- Structural design pattern/Facade Design Pattern/Client.java
+++ Structural design pattern/Facade Design Pattern/Client.java
@@ -1,3 +1,4 @@
+//Problem: Starting a computer requires interacting with several subsystems.
 public class Client {
     public static void main(String[] args) {
         ComputerFacade computer = new ComputerFacade();
