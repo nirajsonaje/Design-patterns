616dcf542ee1bef614395b8c62dba3183d1fa257
Niraj sonaje <101004260+nirajsonaje@users.noreply.github.com>
2025-07-24 17:39:40 +0530

    Create UPIPayment.java

---
+++ behavioral design pattern/Strategy Design Pattern/UPIPayment.java
@@ -0,0 +1,12 @@
+public class UPIPayment implements PaymentStrategy {
+    private String upiId;
+
+    public UPIPayment(String upiId) {
+        this.upiId = upiId;
+    }
+
+    @Override
+    public void pay(double amount) {
+        System.out.println("Paid ₹" + amount + " using UPI: " + upiId);
+    }
+}
