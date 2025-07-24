261dc766494ef146c1143f1e904c0c8e10060f16
Niraj sonaje <101004260+nirajsonaje@users.noreply.github.com>
2025-07-24 17:34:53 +0530

    Create ComputerFacade.java

---
+++ Structural design pattern/Facade Design Pattern/ComputerFacade.java
@@ -0,0 +1,18 @@
+class ComputerFacade {
+    private CPU cpu;
+    private Memory memory;
+    private HardDrive hardDrive;
+
+    public ComputerFacade() {
+        this.cpu = new CPU();
+        this.memory = new Memory();
+        this.hardDrive = new HardDrive();
+    }
+
+    public void startComputer() {
+        cpu.freeze();
+        String bootData = hardDrive.read(100, 4096);
+        memory.load(0, bootData);
+        cpu.execute();
+    }
+}
